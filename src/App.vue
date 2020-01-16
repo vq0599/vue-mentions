14e8b142dd2715246e901b1b91a5142806d28706
dehaohuang <dehaohuang@tencent.com>
2020-01-16 22:50:46 +0800

    :sparkles: basic project schema

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="http://mk.oa.com/img/logo.072fb5dc.svg">
+    <div><Textarea /></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {},
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  height: 1500px;
+
+  img {
+    margin-bottom: 50px;
+  }
+}
+</style>
